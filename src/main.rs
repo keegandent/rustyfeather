33d245f804657330a2d5c6e27fe71860d1ec8cb0
Keegan Dent <keegandent@gmail.com>
2025-04-01 18:43:57 -0500

    say hi

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #![no_std]
 #![no_main]
+// required to resolve linker error
+extern crate cortex_m;
 
+use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use panic_halt as _;
+use rtt_target::{debug_rtt_init_print as rtt_init_print, debug_rprintln as rprintln};
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    rtt_init_print!();
+    loop {
+        rprintln!("Hello, world!");
+        for _ in 0..(1e6 as i32) { nop(); }
+    }
 }
