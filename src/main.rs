fbcb28b147857fb31147700e089b5b1e1f48f0f5
Keegan Dent <keegandent@gmail.com>
2025-04-03 22:36:17 -0500

    it's dancing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 extern crate cortex_m;
 
 use cortex_m_rt::entry;
-use adafruit_nrf52840_express::{Board, prelude::*};
+use adafruit_nrf52840_express::{Board, prelude::*, hal::{pwm::Channel, gpio}};
 use panic_halt as _;
 use rtt_target::{debug_rtt_init_print as rtt_init_print, debug_rprintln as rprintln};
 
@@ -16,10 +16,21 @@ fn main() -> ! {
     let mut delay = board.delay;
 
     let mut led = board.blue_led;
+    let servo_pin = board.d5.into_push_pull_output(gpio::Level::Low);
+    let servo_pwm = board.pwm0;
+    servo_pwm.set_period(50u32.hz());
+    servo_pwm.set_output_pin(Channel::C0, servo_pin.degrade());
     loop {
-        // can technically be accomplished with //.toggle().unwrap() but nice to see other methods
         let new_state = led.is_set_low().unwrap();
         led.set_state(new_state.into()).unwrap();
+        if new_state {
+            // for duty in 0..servo_pwm.get_max_duty() {
+            servo_pwm.set_duty_on_common(servo_pwm.get_max_duty() / 2);
+            //     delay.delay_ms(50u16);
+            // }
+        } else {
+            servo_pwm.set_duty_on_common(servo_pwm.get_max_duty() / 8);
+        }
         delay.delay_ms(500u16);
     }
 }
