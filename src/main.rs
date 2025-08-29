93b3de441f906ad23b7112e540c1867507650991
Keegan Dent <keegandent@gmail.com>
2025-08-28 21:01:40 -0500

    defmt

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
 #![no_std]
 #![no_main]
-// required to resolve linker error
-extern crate cortex_m;
 
+use cortex_m as _;
 use cortex_m::asm::nop;
 use cortex_m_rt::entry;
+use defmt::info;
+use defmt_rtt as _;
 use embedded_hal::digital::{OutputPin, StatefulOutputPin};
 use hal::pac::Peripherals;
 use nrf52840_hal::{self as hal, gpio::Level};
 use panic_halt as _;
-use rtt_target::{debug_rprintln as rprintln, debug_rtt_init_print as rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
     let p = Peripherals::take().unwrap();
     let port1 = hal::gpio::p1::Parts::new(p.P1);
     let mut led = port1.p1_10.into_push_pull_output(Level::Low);
     loop {
-        // can technically be accomplished with //.toggle().unwrap() but nice to see other methods
         let new_state = led.is_set_low().unwrap();
+        info!("Turning {}...", new_state.then_some("on").unwrap_or("off"));
         led.set_state(new_state.into()).unwrap();
-        for _ in 0..(1e6 as i32) {
+        for _ in 0..(5e6 as i32) {
             nop();
         }
     }
