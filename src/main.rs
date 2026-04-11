ddca0753dfcd14e9690d789259aed044f0e4d0dd
Keegan Dent <keegandent@gmail.com>
2026-04-11 03:02:09 -0500

    softdev

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 #![no_std]
 #![no_main]
 
+use core::mem;
+
 use defmt::info;
 use defmt_rtt as _;
 use embassy_executor::{main, Spawner};
 use embassy_nrf::gpio::{Input, Pull};
+use embassy_nrf::interrupt::Priority;
 use embassy_time::Timer;
+use nrf_softdevice::{raw, Softdevice};
 use panic_probe as _;
 
+#[embassy_executor::task]
+async fn softdevice_task(sd: &'static Softdevice) -> ! {
+    sd.run().await
+}
+
 #[main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) -> ! {
+    let mut nrf_config = embassy_nrf::config::Config::default();
+    nrf_config.gpiote_interrupt_priority = Priority::P2;
+    nrf_config.time_interrupt_priority = Priority::P2;
+    let p = embassy_nrf::init(nrf_config);
+
+    let config = nrf_softdevice::Config {
+        ..Default::default()
+    };
+    let sd = Softdevice::enable(&config);
+    spawner.spawn(softdevice_task(sd)).unwrap();
+
     info!("Starting...");
-    let p = embassy_nrf::init(Default::default());
     let mut button = Input::new(p.P1_02, Pull::Up);
     let debounce = async || Timer::after_millis(50).await;
     loop {
